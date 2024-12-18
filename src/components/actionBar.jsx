f61b4e1077f2f23cb22e4f564220d84ba7482263
CasChrist <sozurukovmaksim@gmail.com>
2024-12-18 22:09:43 +0700

    Fix share actions styling bugs

--- src/components/actionBar.jsx
+++ src/components/actionBar.jsx
@@ -1,20 +1,34 @@
-import React from "react";
-import ShareTask from "./ShareTask";
+import React, { useState } from "react";
+import ShareTask from "./shareTask";
 
 const ActionBar = ({ taskID, onEdit }) => {
+  const [showShareTask, setShowShareTask] = useState(false);
+
+  const toggleShareTask = () => {
+    setShowShareTask(!showShareTask);
+  };
+
   return (
-    <div className="tasks-elem-container__actions">
-      <ShareTask taskID={taskID} />
-      <button
-        className="tasks-elem-container__actions-details"
-        onClick={() => alert(`Details for task ID: ${taskID}`)}
-      >
-        i
-      </button>
-      <button className="tasks-elem-container__actions-edit" onClick={onEdit}>
-        <img src="./src/assets/edit.svg" alt="Edit" />
-      </button>
-    </div>
+    <>
+      <div className="tasks-elem-container__actions">
+        <button
+          className="tasks-elem-container__actions-details"
+          onClick={() => alert(`Details for task ID: ${taskID}`)}
+        >
+          i
+        </button>
+        <button className="tasks-elem-container__actions-edit" onClick={onEdit}>
+          <img src="./src/assets/edit.svg" alt="Edit" />
+        </button>
+        <button className="tasks-elem-container__actions-share" onClick={toggleShareTask}>
+          <img src="./src/assets/share.svg" alt="Share" />
+        </button>
+      </div>
+
+      {showShareTask && (
+        <ShareTask taskID={taskID} onClose={toggleShareTask} />
+      )}
+    </>
   );
 };
 
